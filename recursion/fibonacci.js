9147dd0e6691b4910cc130a129c5d645cdc732bf
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-08-16 20:04:27 -0600

    Added solution in iterative and recursive way

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,8 +1,27 @@
-function fibsRec(n) {
-    if (n === 0) {
-        return 0;
+function fibonacci(n) {
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        if (i < 2) {
+            arr.push(i);
+        } else {
+            arr.push(arr[i - 1] + arr[i - 2]);
+        }
     }
+    return arr;
 }
 
-//[0, 1, 1, 2, 3, 5, 8, 13].
-console.log(fibsRec(8));
+function fibonacciRec(n) {
+    if (n === 0) {
+        return [0];
+    } else if (n === 1) {
+        return [0, 1];
+    }
+
+    let arr = fibonacciRec(n - 1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+
+    return arr;
+}
+
+console.log(fibonacciRec(8));
