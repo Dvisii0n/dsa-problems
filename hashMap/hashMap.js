36d6507f8443e16223b4d40b548a25a61612736e
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-08-25 22:58:31 -0600

    Added set function

--- hashMap/hashMap.js
+++ hashMap/hashMap.js
@@ -3,7 +3,8 @@ import LinkedList from "../linkedList/linkedList.js";
 export default class HashMap {
     constructor() {
         this.loadFactor = 0.8;
-        this.capacity = Array.from({ length: 16 });
+        this.capacity = 16;
+        this.buckets = Array.from({ length: this.capacity }, (v, k) => null);
     }
 
     hash(key) {
@@ -13,9 +14,44 @@ export default class HashMap {
         for (let i = 0; i < key.length; i++) {
             hashcode =
                 (primeNumber * hashcode + key.charCodeAt(i)) %
-                this.capacity.length;
+                this.buckets.length;
         }
 
         return hashcode;
     }
+
+    set(key, value) {
+        const hashCode = this.hash(key);
+
+        const bucketList = this.buckets[hashCode];
+
+        if (!bucketList) {
+            this.buckets[hashCode] = new LinkedList();
+            const linkedList = this.buckets[hashCode];
+            linkedList.append([key, value]);
+        } else {
+            this.#replaceOrAppendValue(
+                bucketList,
+                bucketList.head(),
+                key,
+                value
+            );
+        }
+    }
+
+    #replaceOrAppendValue(list, node, key, value) {
+        const storedKey = node.value[0];
+
+        if (key === storedKey) {
+            return (node.value[1] = value);
+        }
+
+        if (!node.nextNode) {
+            return list.append([key, value]);
+        }
+
+        return this.#replaceOrAppendValue(list, node.nextNode, key, value);
+    }
+
+    get(key) {}
 }
---
+++ hashMap/main.js
@@ -0,0 +1,11 @@
+import HashMap from "./hashMap.js";
+
+const hashMap = new HashMap();
+
+hashMap.set("Rama", "this");
+
+hashMap.set("Sita", "this");
+
+hashMap.set("Habibi", "this");
+
+console.log(hashMap.buckets);
