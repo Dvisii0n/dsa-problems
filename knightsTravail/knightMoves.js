6ffe07ddc3ae7f526bd527d79eb3bc401e2756c5
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-09-07 20:38:26 -0600

    Finished solution, lol

--- knightsTravail/knightMoves.js
+++ knightsTravail/knightMoves.js
@@ -1,4 +1,14 @@
+function inRange(x, min, max) {
+    return x >= min && x <= max;
+}
+
 function knightMoves(start, end) {
+    const min = 0;
+    const max = 7;
+    if (!inRange(start[0], min, max) || !inRange(end[0], min, max)) {
+        throw "ERROR: Invalid postion, values must be from 0 to 7";
+    }
+
     const moves = [
         [-1, 2],
         [-2, 1],
@@ -12,29 +22,70 @@ function knightMoves(start, end) {
 
     const queue = [];
 
-    queue.push(start);
+    const rootNode = { value: start, parent: null };
+
+    queue.push(rootNode);
 
     const enqueueMoves = (start, end, queue) => {
         console.log(queue);
-        if (queue[0].toString() === end.toString()) {
-            return;
+        if (queue[0].value.toString() === end.toString()) {
+            return queue[0];
         }
 
         queue.shift();
 
         for (let move of moves) {
-            const validMove = [start[0] + move[0], start[1] + move[1]];
+            const validMove = [
+                start.value[0] + move[0],
+                start.value[1] + move[1],
+            ];
             const x = validMove[0];
             const y = validMove[1];
-            if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
-                queue.push(validMove);
+            if (inRange(x, min, max) && inRange(y, min, max)) {
+                queue.push({ value: validMove, parent: start });
             }
         }
 
         return enqueueMoves(queue[0], end, queue);
     };
 
-    return enqueueMoves(start, end, queue);
+    const reconstructPath = (node) => {
+        if (node.parent === null) {
+            return [node.value];
+        }
+
+        return [node.value].concat(reconstructPath(node.parent));
+    };
+
+    const reversePath = (path) => {
+        let arr = [];
+        for (let i = path.length - 1; i >= 0; i--) {
+            arr.push(path[i]);
+        }
+
+        return arr;
+    };
+
+    const endNode = enqueueMoves(rootNode, end, queue);
+    const path = reconstructPath(endNode);
+    const reversedPath = reversePath(path);
+
+    const nMoves = path.length - 1;
+
+    if (nMoves === 0) {
+        console.log(`You are already here!: `);
+    } else
+        console.log(
+            `You made it in ${reversedPath.length - 1} ${
+                reversedPath.length === 1 ? "move" : "moves"
+            }! Here's your path:`
+        );
+
+    reversedPath.forEach((node) => {
+        console.log(node);
+    });
+
+    return;
 }
 
-knightMoves([0, 0], [3, 3]);
+knightMoves([0, 0], [7, 7]);
